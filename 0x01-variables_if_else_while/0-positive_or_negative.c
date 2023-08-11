6143223035ca3de3fdd24909fc3b390fed58a34e
destinyOrji <destinyorji18@gmail.com>
2023-08-11 12:35:26 -0700

    This program will assign a random number to the variable n each time it is executed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-/* more headers goes there */
-/* betty style doc for function main goes there */
+/**
+* main - the start of the program main function
+*
+* Return: Always return zero
+*
+* betty style doc for function main goes there
+*/
 int main(void)
 {
 int n;
