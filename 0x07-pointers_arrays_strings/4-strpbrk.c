630cbfacd0971c27236adce5749e5f88dc518b60
destinyOrji <destinyorji18@gmail.com>
2023-08-29 09:13:36 -0700

    a function that searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  * _strpbrk - Entry point
  * @s: input
  * @accept: input
  * Return: Always 0 (Success)
  */
+
 char *_strpbrk(char *s, char *accept)
 {
 int k;
@@ -13,7 +15,7 @@ while (*s)
 for (k = 0; accept[k]; k++)
 {
 if (*s == accept[k])
-}return (s);
+return (s);
 }
 s++;
 }
