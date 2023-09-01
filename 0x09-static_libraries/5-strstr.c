b3d0695fd5ccfc9e9eedea72ff08a68dfee9e314
destinyOrji <destinyorji18@gmail.com>
2023-09-01 14:12:01 -0700

    Create the static library libmy.a containing all the functions listed below

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+for (; *haystack != '\0'; haystack++)
+{
+char *i = haystack;
+char *k = needle;
+while (*i == *k && *k != '\0')
+{
+i++;
+k++;
+}
+if (*k == '\0')
+return (haystack);
+}
+return (0);
+}
